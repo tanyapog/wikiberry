ddd9473e116a25fe93be01da2455fbce3c214dc7
tanyapog <megatatb@yandex.ru>
2024-02-22 14:07:07 +0300

    app icons

---
+++ lib/presentation/shared/theme/app_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppIcon {
+  AppIcon._();
+
+  static const _kFontFam = 'AppIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData skull = IconData(0xf54c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData skullCrossbones = IconData(0xf714, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData raspberry = IconData(0xf7bb, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
